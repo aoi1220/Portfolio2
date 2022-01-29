1d669c246db8f1e0371367045f7ded684b20c95d
aoi1220 <67041953+aoi1220@users.noreply.github.com>
2022-01-29 14:35:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ $(function (){
     })
   })
   });
+
+(function () {
+  var today = new Date();
+  document.querySelectorAll('[data-birth]').forEach(function (element) {
+  var birth = element.getAttribute('data-birth');
+  if (birth.match(/^\d{4}(-|\/)\d{2}(-|\/)\d{2}$/)) {
+  var birthDate = new Date(birth);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+  age--;
+  }
+  age = Math.max(age, 0);
+  element.innerHTML = age;
+  }
+  });
+  })();
